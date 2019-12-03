d97ae408fb363cd85338675e20a540af3e4aa32d
Sehbaz Rafik <sehbazm@gmail.com>
2019-12-03 02:10:07 +0000

    about page update

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -7,8 +7,24 @@ import Footer from "../components/footer"
 
 const AboutPage = () => (
   <Layout>
-    <div>
+    <div style={{ fontFamily: `Helvetica`, height: `60vh` }}>
       <h1 style={{ paddingTop: `2rem` }}>About</h1>
+      <p>
+        I am Sehbaz Rafik a developer 🛠 who is keen and passionate about
+        building Web Based applications and who has experience in a variety of
+        languages and technologies.
+      </p>
+      <p>
+        {" "}
+        I am always looking to increasing my skills set, with one of my greatest
+        strengths being the ability to quickly learn and become proficient in
+        technologies that are new to me.
+      </p>
+      <p>
+        {" "}
+        I am an active contributor to open source projects, and I am a member of
+        the LeicesterJS meetup group.
+      </p>
     </div>
 
     <Footer></Footer>
